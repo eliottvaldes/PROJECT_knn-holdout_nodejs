05fb9bb220d71cdccc13e915ac60211eb6e4e051
Eliot Valdes <eliottvaldes@hotmail.com>
2023-06-19 01:20:36 -0600

    feat: define endpoints for api/setup/

--- routes/setup.routes.js
+++ routes/setup.routes.js
@@ -1,12 +1,55 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { validateFields } = require('../middlewares/validateFiles');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        msg: 'GET | setup'
-    });
-});
+// call the controller to get allData
+const { getAllData, getClasses, getTainingAndTestingData } = require('../controllers/setupDataset.controller');
+
+/** 
+ * @route GET api/setup/all-data
+ * @desc get all the data from the dataset file
+ * @access Public
+ * @returns {object} 200 | 400 - Bad request | 500 - Server error
+*/
+router.get('/all-data',
+    getAllData
+);
+
+/**
+ * @route GET api/setup/unique-classes
+ * @desc get the unique classes from the dataset file
+ * @access Public
+ * @param {object} irisData - the data from the dataset file
+ * @returns {object} 200 | 400 - Bad request | 500 - Server error
+ */
+router.get('/unique-classes',
+    [
+        check('irisData', 'The irisData is required').not().isEmpty(),
+        validateFields
+    ],
+    getClasses
+);
+
+/**
+ * @route POST api/setup/training-testing-data
+ * @desc separate the data into training and testing data
+ * @access Public
+ * @param {object} irisData - the data from the dataset file
+ * @param {object} uniqueClasses - the unique classes from the dataset file
+ * @returns {object} 200 | 400 - Bad request | 500 - Server error
+ */
+router.post('/training-testing-data',
+    [
+        check('irisData', 'The irisData is required').not().isEmpty(),
+        validateFields,
+        check('uniqueClasses', 'The uniqueClasses is required').not().isEmpty(),
+        validateFields,
+    ],
+    getTainingAndTestingData
+);
 
 
 module.exports = router;
