b8a105b2e97be33b586adfa0639092bf2c74c8f2
Eliot Valdes <eliottvaldes@hotmail.com>
2023-06-18 22:54:34 -0600

    feat: create the model for express server

---
+++ models/server.model.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 5000;
+
+        // define the paths for routes
+        this.paths = {
+            setup:   '/api/setup',
+        }
+
+        // run the middlewares and routes
+        this.middlewares();
+        this.routes();
+    }
+
+
+    // middlewares definition
+    middlewares() {
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(express.static('public'));
+    }
+
+    // routes definition
+    routes() {
+        // define the routes to use each path value
+        this.app.use(this.paths.setup, require('../routes/setup.routes'));
+
+        // 404 error
+        this.app.use('*', (req, res) => {
+            res.status(404).json({
+                msg: '404 error | Resource not found'
+            });
+        });
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Server is running on http://localhost:${this.port}`);
+        });
+    }
+
+}
+
+module.exports = Server;
