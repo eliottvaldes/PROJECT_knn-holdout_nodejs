053bcdc3f12d6279cb3fd424fe6fb619d275ce85
Eliot Valdes <eliottvaldes@hotmail.com>
2023-06-18 23:47:41 -0600

    feat: f' to separate dataset into training and test data

--- helpers/datasetTreatment.js
+++ helpers/datasetTreatment.js
@@ -35,10 +35,42 @@ const readFile = async (fullPath) => {
 const getUniqueClasses = (irisData) => [...new Set(irisData.map(item => item.class))];
 
 
+// function to separate the irisData in arrays: training and test data
+// the training data is 70% of the irisData and the test data is 30% of the irisData
+// the training data must be separated by class in the same proportion
+// the separation is done using the unique classes of the irisData independently
+const separateData = (irisData, uniqueClasses) => {
+    // define the arrays to store the training and test data
+    const trainingData = [];
+    const testData = [];
+
+    // loop through the unique classes
+    uniqueClasses.forEach((uniqueClass) => {
+        // filter the irisData by class
+        const irisDataByClass = irisData.filter(item => item.class === uniqueClass);
+        // calculate the 70% of the irisDataByClass
+        const trainingDataByClassLength = Math.round(irisDataByClass.length * 0.70);
+
+        // loop through the irisDataByClass
+        irisDataByClass.forEach((item, index) => {
+            // if the index is less than the trainingDataByClassLength
+            if (index < trainingDataByClassLength) {
+                // push the item into the trainingData array
+                trainingData.push(item);
+            } else {
+                // push the item into the testData array
+                testData.push(item);
+            }
+        })
+    })
+
+    // return the training and test data
+    return { trainingData, testData };
+}
 
 
 module.exports = {
     readFile,
     getUniqueClasses,
-    
+    separateData,
 }
