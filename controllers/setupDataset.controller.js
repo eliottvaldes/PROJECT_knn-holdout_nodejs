151db8849f2e1409c21f2e3b192946aa79ab817d
Eliot Valdes <eliottvaldes@hotmail.com>
2023-06-19 01:05:00 -0600

    feat: f' to return the uniqueclasses

--- controllers/setupDataset.controller.js
+++ controllers/setupDataset.controller.js
@@ -4,14 +4,14 @@ const { request, response } = require("express");
 // require the helpers
 const { readFile, getUniqueClasses, separateData } = require('../helpers/datasetTreatment');
 
+// Define the fileName and the path to get the data
+let fileName = '';
+fileName = '/../assets/files/iris.data';
+fileName = '/../assets/files/iris-test.data';
+const fullPath = __dirname + fileName;
+
+
 const getAllData = async (req = request, res = response) => {
-
-    // hardcode to define the fileName and the path
-    let fileName = '';
-    fileName = '/../assets/files/iris.data';
-    fileName = '/../assets/files/iris-test.data';
-    const fullPath = __dirname + fileName;
-
 
     // read the file using the helpers
     try {
@@ -37,7 +37,36 @@ const getAllData = async (req = request, res = response) => {
 
 }
 
+const getClasses = async (req = request, res = response) => {
+
+    // get the irisData values from the request body in a property called irisData
+    const { irisData } = req.body;
+
+    try {
+
+        // get the unique classes using the helpers
+        const uniqueClasses = await getUniqueClasses(irisData);
+
+        // return the response with the uniqueClasses
+        res.status(200).json({
+            ok: true,
+            uniqueClasses
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        // return the response with the error message
+        res.status(500).json({
+            ok: false,
+            msg: 'Error getting the unique classes'
+        });
+    }
+
+}
+
 
 module.exports = {
     getAllData,
+    getClasses,
 }
